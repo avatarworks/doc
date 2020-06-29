cceb6f3a2851c644f5550da3d74b3af964c133aa
avatarworks <59176287+avatarworks@users.noreply.github.com>
2020-06-29 17:13:16 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -68,5 +68,9 @@ html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 source_suffix = ['.rst', '.md']
 master_doc = 'index'
