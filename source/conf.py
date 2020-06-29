83f180a0eb615417840ab349ee68dac236f128c8
avatarworks <59176287+avatarworks@users.noreply.github.com>
2020-06-29 18:00:35 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -76,3 +76,16 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 #source_suffix = ['.rst', '.md']
 master_doc = 'index'
+
+
+
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+        #'url_resolver': lambda url: github_doc_root + url,
+        'auto_toc_tree_section': 'Contents',
+        'enable_math': False,
+        'enable_inline_math': False,
+        'enable_eval_rst': True,
+        'enable_auto_doc_ref': True,
+    }, True)
+    app.add_transform(AutoStructify)
